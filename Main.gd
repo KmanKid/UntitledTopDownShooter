8447befddad1d4e49cc596a1c2879faa605bee38
RealSnart <tbreier77@gmail.com>
2023-06-18 20:46:30 +0200

    Menu
    added main menu

--- Main.gd
+++ Main.gd
@@ -6,7 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.player_fired_bullet.connect(bullet_manager.handle_bullet_spawned)
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
